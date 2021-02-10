368018f312fa9e81e98c5d3f2f76b118bd00f7be
deepen1987 <deepen1987@gmail.com>
2021-02-10 18:35:39 -0500

    Create main.py

---
+++ School/Project/Tic_Tac_Toe/main.py
@@ -0,0 +1,9 @@
+
+# Compare for a "X" or "O" pattern after each user input make a function call.
+# if list is full return its a draw
+# if pattern matches declare a winner
+#
+# Take input based on position --> if player 1 then "X" and if player 2 then "O".
+# Exception handling for user input
+# Use list to store elements.
+# Make use of loops, class, functions and collections
